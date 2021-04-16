798bb0c410b073b9f10a5a40689c5a85ca0cea0e
Rizal Rasyadan <rizalrasyadan03@gmail.com>
2021-04-17 02:16:23 +0700

    Rizal

---
+++ I0320086_Soal1_Tugas7.py
@@ -0,0 +1,15 @@
+# Metode center
+Judul = "Survei menonton sepak bola"
+a = Judul.center (50,'=')
+print (a)
+
+# Metode endswith
+str = "Apakah pemain bola kuat?"
+print("Apakah pemain bola kuat?")
+print (str.endswith("kuat?"))
+
+#Metode replace dan endswith
+str = "Apakah penontonnya kompak?"
+b = str.replace ("kompak","solidaritas")
+print(b)
+print(str.endswith("kompak?"))
---
+++ I0320086_Soal2_Tugas7.py
@@ -0,0 +1,19 @@
+# Fungsi floor
+import math
+nilai = 77
+print ("Rizal RH mempunyai nilai kalkulus sebesar", math.floor(nilai), "pada ujian akhir semester 1")
+
+# Fungsi min dan max
+import math
+MatTek = 77
+Fisika = 87
+Biologi = 92
+print ("Nilai tertinggi yang Rizal RH dapatkan pada semester ini ialah ", max(MatTek,Fisika,Biologi), ", yaitu pada matkul Biologi")
+print ("Nilai terendah yang Rizal RH dapatkan pada semester ini ialah ", min(MatTek,Fisika,Biologi), ", yaitu pada matkul MatTek")
+
+# Fungsi sqrt
+import math
+Alas = 40
+Tinggi = 20
+S_miring = Alas ** 2 + Tinggi ** 2
+print ("Rizal RH diberi tugas oleh Pak Wayan untuk menghitung sisi miring lapangan fakultas teknik, \nhasil pengamatannya menyatakan bahwa besar sisi miring lapangan fakultas teknik ialah",math.sqrt(S_miring),"m")
